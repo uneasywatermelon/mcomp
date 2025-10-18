bbf578c93ade6cb3ae83547e2170a45e9821d0dc
uneasywatermelon <marioalemany16@gmail.com>
2025-10-17 17:42:42 -0700

    updated test script

--- __out.c
+++ __out.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-int main() {int x = 5;int y = 10;char* j = "John";for (int __iter = 0; __iter < (y-x); __iter++) {printf("%s \n", j);}return 0;}
+int main() {int x = 15;int y = 4;for (int __iter = 0; __iter < (x * y); __iter++) {printf("Hello \n");}return 0;}
