7edfbbdd8eb81a1cf37748b4e045408e20bb3133
uneasywatermelon <uneasywatermelon@users.noreply.github.com>
2025-10-18 18:59:23 -0700

    set up for observation, must add array init and ranges for printing, will be very similar logic. will be cool.

--- __out.c
+++ __out.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-int main() {int x = 15;int y = 4;for (int __iter = 0; __iter < (x * y); __iter++) {printf("Hello \n");}return 0;}
+int main() {int x = 15;int y = 4;char* greeting = "Hello";for (int __i = 0; __i < (x * y); __i++) {printf("%s \n", greeting);}return 0;}
